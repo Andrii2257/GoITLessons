39139089b42d1ed30e9aac38e89e2144a633f8ed
Andrii2257 <a.g.kovalenko1@gmail.com>
2023-03-09 23:26:14 +0200

    add hw3

---
+++ src/main/java/ua/goit/Module11/hw1/NamesOrder.java
@@ -0,0 +1,4 @@
+package ua.goit.Module11.hw1;
+
+public class NamesOrder {
+}
---
+++ src/main/java/ua/goit/Module12/hw1/TimeCounter.java
@@ -0,0 +1,20 @@
+package ua.goit.Module12.hw1;
+
+import java.time.LocalTime;
+import java.util.concurrent.*;
+
+public class TimeCounter {
+    private  static int secondsFromStart = 0;
+    public static void main(String[] args) {
+        System.out.println("Start " + LocalTime.now());
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
+        executorService.scheduleAtFixedRate(()->{
+            System.out.println("После запуска прошло " + ++secondsFromStart + " секунд " + LocalTime.now());
+        }, 1, 1, TimeUnit.SECONDS);
+
+        executorService.scheduleAtFixedRate(()->{
+            System.out.println("Прошло 5 секунд " + LocalTime.now());
+        }, 5, 5, TimeUnit.SECONDS);
+    }
+}
